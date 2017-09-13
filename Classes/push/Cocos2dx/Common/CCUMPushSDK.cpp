8f465a2c0a01ea603eb9357ed4bf7478c9c8f864
wf90163 <wf90163@alibaba-inc.com>
2017-09-13 09:49:56 +0800

    fix
    Former-commit-id: 0b8706c7bd26f31a2fbd55143eac593015b5d560

--- Classes/push/Cocos2dx/Common/CCUMPushSDK.cpp
+++ Classes/push/Cocos2dx/Common/CCUMPushSDK.cpp
@@ -42,7 +42,7 @@ void CCUMPushSDK::initSDK() {
 
 #endif
 }
-CCUMPushSDK* CCUMPushSDK::create() {
+ CCUMPushSDK* CCUMPushSDK::create() {
     
     if (_instance == NULL) {
         _instance = new CCUMPushSDK();
